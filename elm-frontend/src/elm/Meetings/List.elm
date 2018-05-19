81f5674abde1066d328fe4ba047e56e33daff907
David Kowis <david@kow.is>
2018-05-19 14:30:48 -0500

    Getting the imported button to only show up when it's supposed to

--- elm-frontend/src/elm/Meetings/List.elm
+++ elm-frontend/src/elm/Meetings/List.elm
@@ -2,7 +2,7 @@ module Meetings.List exposing (..)
 
 import Date exposing (Date)
 import Html exposing (..)
-import Html.Attributes exposing (class, href)
+import Html.Attributes exposing (class, href, type_)
 import Msgs exposing (Msg)
 import Models exposing (MeetingDetails)
 import RemoteData exposing (WebData)
@@ -41,13 +41,25 @@ list meetingDetails =
 
 detailsRow : MeetingDetails -> Html Msg
 detailsRow details =
-  a [href ("#meetings/" ++ details.meetingId), class "list-group-item" ]
-    [ h5 [class "mb-1"] [ text ((dateFormat details.meetup.date) ++ " " ++ details.meetup.topic) ]
-    , p [class "mb-1" ] [ text ((toString (List.length details.attendees)) ++ " Attendees") ]
-    ]
+  div [class "list-group-item" ]
+    (importButton details [
+      a [href ("#meetings/" ++ details.meetingId) ]
+        [ h5 [class "mb-1"] [ text ((dateFormat details.meetup.date) ++ " " ++ details.meetup.topic) ]
+        , p [class "mb-1" ] [ text ((toString (List.length details.attendees)) ++ " Attendees") ]
+        ]
+    ])
 
 dateFormat : Date -> String
 dateFormat date =
   (toString (Date.day date)) ++ "-" ++
     (toString (Date.month date)) ++ "-" ++
     (toString (Date.year date))
+
+importButton : MeetingDetails -> List (Html Msg) -> List (Html Msg)
+importButton details further =
+  if details.imported then
+    further
+  else
+    --Have to look at the status of the imported maybe
+    
+    (button [class "btn btn-primary import_button", type_ "button"] [text "IMPORT"] ) :: further
