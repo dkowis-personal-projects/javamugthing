53d4be39622162c163012178df4bfd9248af4ace
David Kowis <david@kow.is>
2018-06-20 15:51:49 -0500

    Work in progress

--- backend/src/main/java/org/javamug/meetuptool/meetuptool/giveaway/GiveawayPrizeController.java
+++ backend/src/main/java/org/javamug/meetuptool/meetuptool/giveaway/GiveawayPrizeController.java
@@ -6,40 +6,18 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 
 @RestController
-@RequestMapping("/meetings/{meetingId}/giveaway")
-public class GiveawayController {
-
-    @GetMapping("/")
-    @ResponseBody
-    public Mono<GiveawayDetails> giveawayDetails(@RequestParam("meetingId") String meetingId) {
-        return Mono.empty();
-    }
-
-    @PostMapping("/")
-    @ResponseBody
-    public Mono<GiveawayDetails> startGiveaway(
-            @RequestParam("meetingId") String meetingId,
-            @RequestBody GiveawayTrigger payload) {
-        return Mono.empty();
-    }
-
-    @PutMapping("/")
-    @ResponseBody
-    public Mono<GiveawayDetails> completeGiveaway(
-            @RequestParam("meetingId") String meetingId,
-            @RequestBody GiveawayTrigger payload) {
-        return Mono.empty();
-    }
+@RequestMapping("/meetings/{meetingId}/giveaway/prizes")
+public class GiveawayPrizeController {
 
     //prizes for this giveaway
-    @GetMapping("/prizes")
+    @GetMapping("/")
     @ResponseBody
     public Mono<List<GiveawayPrize>> listPrizes(
             @RequestParam("meetingId") String meetingId) {
         return Mono.empty();
     }
 
-    @PostMapping("/prizes")
+    @PostMapping("/")
     @ResponseBody
     public Mono<GiveawayPrize> addPrize(
             @RequestParam("meetingId") String meetingId,
@@ -47,7 +25,7 @@ public class GiveawayController {
         return Mono.empty();
     }
 
-    @PutMapping("/prizes/{prizeId}")
+    @PutMapping("/{prizeId}")
     @ResponseBody
     public Mono<GiveawayPrize> addPrize(
             @RequestParam("meetingId") String meetingId,
@@ -56,14 +34,13 @@ public class GiveawayController {
         return Mono.empty();
     }
 
-    @DeleteMapping("/prizes/{prizeId}")
+    @DeleteMapping("/{prizeId}")
     @ResponseBody
     public Mono<GiveawayPrize> removePrize( //TODO: return type, just want a 201
-            @RequestParam("meetingId") String meetingId,
-            @RequestParam("prizeId") String prizeId) {
+                                            @RequestParam("meetingId") String meetingId,
+                                            @RequestParam("prizeId") String prizeId) {
         return Mono.empty();
     }
-
 
 
 
